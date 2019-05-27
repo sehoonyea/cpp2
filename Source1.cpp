6d7ecb48618683b36aa215ba3b2a307bd01510c4
Sehoon Yea <yeamerl@gmail.com>
2019-05-26 20:37:45 -0700

    one line change for test

--- Source1.cpp
+++ Source1.cpp
@@ -12,7 +12,7 @@ int main()
 	vector<int> array2(10);
 	vector<int> array3(10, 1);
 	vector<int> array4(array3);
-	int data[] = { 10,20,30,40,50,60 };
+	int data[] = { 10,20,30,40,50,60,70 };
 	vector<int> array5(data, data + 5);
 	array5.insert(array5.begin(), 99);
 
